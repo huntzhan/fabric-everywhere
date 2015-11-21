5c8a5be9b15f227c48d9aef2b9b4722eae1260da
huntzhan <programmer.zhx@gmail.com>
2015-11-21 17:58:10 +0800

    Init.

---
+++ fabric_everywhere/metadata.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+NAME = 'fabric_everywhere'
+VERSION = '0.1.0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'A tool to organize fabfiles.'
